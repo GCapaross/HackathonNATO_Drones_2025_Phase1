14c97c3f85a455941f619702ece141c2d3304122
GCapaross <119590128+GCapaross@users.noreply.github.com>
2025-09-22 22:52:27 +0100

    Confusion matrix

--- GitHubs/DroneRF-master/Matlab/Demo_4_Classification.m
+++ GitHubs/DroneRF-master/Matlab/Demo_4_Classification.m
@@ -30,10 +30,23 @@
 close all; clear; clc
 % filepath = fileparts(pwd);
 % Only when the confusion matrix gets generated
-filepath = '';
+filepath = 'G:\Programing\HackathonNATO_Drones_2025\GitHubs\DroneRF-master\Python\';
 
 %% Parameters
-opt = 1;  % Change to 1, 2, or 3 to alternate between the 1st, 2nd, and 3rd DNN results respectively.
+opt = 3;  % Change to 1, 2, or 3 to alternate between the 1st, 2nd, and 3rd DNN results respectively.
+%In this MATLAB script, opt is a variable that tells the script which DNN (Deep Neural Network) results you are using. It controls two main things:
+
+%Which columns of the CSV are used
+
+%opt = 1 → uses columns 1–2 for true labels and 3–4 for predictions
+
+%opt = 2 → uses columns 1–4 for true labels and 5–8 for predictions
+
+%opt = 3 → uses columns 1–10 for true labels and 11–20 for predictions
+
+%Which plotting function is called
+
+%Depending on opt, the script calls plotconfusion_mod with the right slices of your CSV data.
 
 %% Main
 y = [];
@@ -41,7 +54,6 @@ for i = 1:10
     x = csvread([filepath 'Results_' num2str(opt) num2str(i) '.csv']);
     y = [y ; x];
 end
-
 %% Plotting confusion matrix
 if(opt == 1)
     plotconfusion_mod(y(:,1:2)',y(:,3:4)');
